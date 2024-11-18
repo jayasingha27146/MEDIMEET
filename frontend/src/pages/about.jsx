fd368d2f7ea05a6e50479f8a09be58689cd49def
limasha sathsara <140880178+jayasingha27146@users.noreply.github.com>
2024-11-18 11:10:12 +0530

    change color

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -12,7 +12,7 @@ const about = () => {
               <img className='w-auto md:max-w-[360px]' src={assets.about_image} alt='' />
             
 
-        <div className='flex flex-col justify-center gap-6 md:w-2/3 text-base text-gray-600'>
+        <div className='flex flex-col justify-center gap-6 md:w-2/3 text-base text-gray-700'>
           <p>Welcome to MediMeet, your trusted partner in simplifying healthcare management. At MediMeet, we understand the challenges individuals face when it comes to scheduling doctor appointments and keeping track of health records. Our mission is to make healthcare accessible, convenient, and stress-free for everyone.</p>
           <p>MediMeet is dedicated to excellence in healthcare technology. We are constantly evolving our platform, integrating the latest innovations to enhance user experience and provide top-tier service. Whether you’re scheduling your first appointment or managing ongoing care, MediMeet is here to support you at every step of your healthcare journey.</p>
           
