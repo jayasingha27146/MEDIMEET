e4e571450f785d29681daf549e386e7e79a13210
limasha sathsara <140880178+jayasingha27146@users.noreply.github.com>
2024-11-19 00:48:29 +0530

    update text

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -17,7 +17,7 @@ const Footer = () => {
         <p className='text-xl font-medium mb-5 text-primary'>COMPANY</p>
           <ul className='flex flex-col gap-2 text-gray-600' >
             
-            <li><a href="/">Home</a></li>
+            <li><a href="/">Home MEDIMEET</a></li>
             <li><a href="/about">About us</a></li>
             <li><a href="/contact">Contact us</a></li>
             <li><a href="">Privacy Policy</a></li>
