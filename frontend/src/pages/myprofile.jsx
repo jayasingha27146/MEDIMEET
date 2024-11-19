91f886e00b347bb4bcf41c82b69fff0c004339b3
limasha sathsara <140880178+jayasingha27146@users.noreply.github.com>
2024-11-19 23:09:35 +0530

    update myprofile page

--- frontend/src/pages/myprofile.jsx
+++ frontend/src/pages/myprofile.jsx
@@ -85,7 +85,7 @@ const myprofile = () => {
         <div className='mt-10'>
           {
             isEdit 
-            ? <button className='border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all' onClick={() => setIsEdit(false)}>Save Your Details</button>
+            ? <button className='border border-primary px-6 py-2 rounded-full hover:bg-primary hover:text-white transition-all' onClick={() => setIsEdit(false)}>Save Your Details</button>
             : <button className='border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all' onClick={() => setIsEdit(true)}>Edit</button>
           }
         </div>
